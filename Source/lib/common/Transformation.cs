804ea10f201750e3034a8c1eeaa02da37f4032b2
Cillié Malan <cilliemalan@gmail.com>
2016-03-01 17:23:55 +0200

    Added Transformation

---
+++ Source/lib/common/Transformation.cs
@@ -0,0 +1,27 @@
+﻿namespace ZXing.Common
+{
+    public abstract class Transformation
+    {
+        public abstract Point Transform(int x, int y);
+        public abstract Point ReverseTransform(int x, int y);
+
+        public virtual Point Transform(Point p) { return Transform(p.X, p.Y); }
+        public virtual Point ReverseTransform(Point p) { return ReverseTransform(p.X, p.Y); }
+
+        public virtual Point[] BulkTransform(Point[] pnts, int offset, int amount, int startX, int y)
+        {
+            for (int x = startX, i = offset; i < amount; i++, x++)
+            {
+                pnts[i] = Transform(x, y);
+            }
+
+            return pnts;
+        }
+
+        public struct Point
+        {
+            public Point(int x, int y) { X = x; Y = y; }
+            public int X, Y;
+        }
+    }
+}
