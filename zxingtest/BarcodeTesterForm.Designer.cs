b620ed143e05a9d4bd095d655005d7918b965a2a
Cillié Malan <cilliemalan@gmail.com>
2016-03-01 11:16:26 +0200

    Added zxingtest temporarily

---
+++ zxingtest/BarcodeTesterForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace zxingtest
+{
+    partial class BarcodeTesterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofdImageSelector = new System.Windows.Forms.OpenFileDialog();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ofdImageSelector
+            // 
+            this.ofdImageSelector.Filter = "Image|*.jpg;*.png";
+            this.ofdImageSelector.Multiselect = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 41);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(443, 302);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // BarcodeTesterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 355);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "BarcodeTesterForm";
+            this.Text = "Barcode Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdImageSelector;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ zxingtest/BarcodeTesterForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace zxingtest
+{
+    public partial class BarcodeTesterForm : Form
+    {
+        public BarcodeTesterForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (ofdImageSelector.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    btnLoad.Text = "Working...";
+                    btnLoad.Enabled = false;
+
+                    foreach (var file in ofdImageSelector.FileNames)
+                    {
+                        txtOutput.Text += $"Testing {Path.GetFileName(file)}...";
+
+                        var result = await Task.Run(() =>
+                        {
+                            using (var img = (Bitmap)Image.FromFile(file))
+                            {
+                                var bcr = new BarcodeReader();
+                                bcr.Options.PossibleFormats = new[] { BarcodeFormat.PDF_417 };
+                                bcr.AutoRotate = true;
+                                return bcr.Decode(img);
+                            }
+                        });
+
+                        if (result != null && !string.IsNullOrEmpty(result.Text))
+                        {
+                            txtOutput.Text += "OK!\r\n";
+                        }
+                        else
+                        {
+                            txtOutput.Text += "BAD\r\n";
+                        }
+                    }
+                }
+                finally
+                {
+                    btnLoad.Text = "Load";
+                    btnLoad.Enabled = true;
+                }
+            }
+        }
+
+        private void Log(string s)
+        {
+            txtOutput.Text += s + "\r\n";
+        }
+    }
+}
