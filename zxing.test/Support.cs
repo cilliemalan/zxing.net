055d754ddc3c02c3ceec00b9bd29bd0cf6dbc331
Cillié Malan <cilliemalan@gmail.com>
2016-03-11 11:22:11 +0200

    Fixing rotation of image.

--- zxing.test/Support.cs
+++ zxing.test/Support.cs
@@ -12,7 +12,46 @@ namespace zxing.test
     {
         public static Bitmap Image(string path)
         {
-            return (Bitmap)Bitmap.FromFile(Path.Combine("../../Images", path));
+            var bmp = (Bitmap)Bitmap.FromFile(Path.Combine("../../Images", path));
+            FixRotation(bmp);
+            return bmp;
+        }
+
+        private static void FixRotation(Bitmap img)
+        {
+            if (Array.IndexOf(img.PropertyIdList, 274) > -1)
+            {
+                var orientation = (int)img.GetPropertyItem(274).Value[0];
+                switch (orientation)
+                {
+                    case 1:
+                        // No rotation required.
+                        break;
+                    case 2:
+                        img.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                        break;
+                    case 3:
+                        img.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        break;
+                    case 4:
+                        img.RotateFlip(RotateFlipType.Rotate180FlipX);
+                        break;
+                    case 5:
+                        img.RotateFlip(RotateFlipType.Rotate90FlipX);
+                        break;
+                    case 6:
+                        img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        break;
+                    case 7:
+                        img.RotateFlip(RotateFlipType.Rotate270FlipX);
+                        break;
+                    case 8:
+                        img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        break;
+                }
+                // This EXIF data is now invalid and should be removed.
+                img.RemovePropertyItem(274);
+            }
         }
     }
 }
