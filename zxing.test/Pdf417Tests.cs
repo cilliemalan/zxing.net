80feb08eedf20fcac03c838a9ca43dc54f801226
Cillié Malan <cilliemalan@gmail.com>
2016-03-11 11:43:05 +0200

    Added test for (fake) license card. Note: License card barcode is flipped vertically for reasons unknown.

--- zxing.test/Pdf417Tests.cs
+++ zxing.test/Pdf417Tests.cs
@@ -61,5 +61,16 @@ namespace zxing.test
 
             Assert.Equal(expected, decoded);
         }
+
+        [Fact]
+        public void LicenseCardTest()
+        {
+            var image = Support.Image("Pdf417/driverslicense_fake.png");
+            var expected = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas imperdiet neque arcu, ac dictum ex commodo quis. Sed iaculis, nibh quis dignissim dictum, turpis ex mattis velit, vel consequat sapien orci et felis. Mauris quis massa convallis, consequat sem feugiat, suscipit purus. Suspendisse at rhoncus turpis, in maximus sapien. Nunc ultricies, nunc sit amet tempus pellentesque, augue magna rutrum ligula, non pharetra sem sapien in ex. Duis ultrices ligula at justo consequat, id fermentum libero rhoncus. Sed in egestas magna, vitae consequat dolor. Mauris sem ligula, posuere ac velit eget, aliquet molestie velit. Nunc tincidunt sem nunc, vel mattis tellus hendrerit id. Vestibulum ut dolor sit amet nisi vehicula sodales a fringilla odio. Cum sociis natoque penatibus et magnis dis parturient montes.";
+
+            string decoded = barcodereader.Decode(image)?.Text;
+
+            Assert.Equal(expected, decoded);
+        }
     }
 }
